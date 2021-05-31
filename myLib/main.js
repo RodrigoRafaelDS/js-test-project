642db973b8f517532e10fecb65de16d30f93bff5
rodrigo-projects <rodrigorrsousa6@gmail.com>
2021-05-31 17:09:12 -0300

    Comentários

--- myLib/main.js
+++ myLib/main.js
@@ -1,9 +1,10 @@
 
 
-const finais_semana = ['sat', 'sun']
+
+// Função principal
 function hotel_escolhido(dadosCliente) {
 
-
+    // Guarda hotel e valor pago pelo cliente
     const cliente = {
         tipo: false,
         dias: [],
@@ -11,6 +12,7 @@ function hotel_escolhido(dadosCliente) {
         valor: 0
     }
 
+    // Guarda informações sobre hoteis
     const hoteis = [{
         nome: 'Lakewood',
         classe: '3',
@@ -32,38 +34,46 @@ function hotel_escolhido(dadosCliente) {
         valor: 0
     }
     ]
-    
+
+    // Clona objetos
     var clienteClone = new Object(cliente)
     var hoteisClone = new Object(hoteis)
 
+    // Retorna o tipo e os dias de hospedagem do cliente 
     clienteClone = avEntrada(dadosCliente, clienteClone)
-    // console.log('clienteClone: ')
-    // console.log(clienteClone)
 
+    // Retorna o valor em cada hotel 
     hoteisClone, clienteClone = valor(hoteisClone, clienteClone)
 
-    // console.log('hoteisClone: ')
-    // console.log(hoteisClone)
-
+    // Retorna o melhor hotel mais barato
     clienteClone = escolha(hoteisClone, clienteClone)
-    var resposta = clienteClone.hotel
-    // console.log('resposta: ')
-    // console.log(clienteClone.hotel)
 
-    return resposta
+    // Imprime resposta 
+    // console.log(cliente.hotel)
+
+    return clienteClone.hotel
 }
 
-function avEntrada(entrada, clienteC) {
 
-    if (entrada.split(":", 1) == 'Regular') {
+// Retorna o tipo e os dias de hospedagem do cliente 
+// Trata a entrada separando as partes de interesse 
+function avEntrada(entrada, clienteC) {
+    // Variável auxiliar
+    const finais_semana = ['sat', 'sun']
+    tipoCliente=entrada.toLowerCase().split(":", 1)
+    // Verifica se o cliente normal ou do programa de fidelidade 
+    if (tipoCliente == 'regular') {
         clienteC.tipo = false
     } else {
         clienteC.tipo = true
     }
 
-    dado1 = entrada.split(" ")
+    // Verifica as datas de hospedagem e separa em finais de semana ou não 
+    //Separa a string para verificar os dias de hospedagem
+    dado1 = entrada.split(",")
 
-    var l = 1
+    //Verifica se o dia de hospedagem é final de semana ou dia de semana
+    var l = 0
     while (l < dado1.length) {
         if (dado1[l].match(finais_semana[0]) || dado1[l].match(finais_semana[1])) {
             clienteC.dias.push(false)
@@ -72,22 +82,35 @@ function avEntrada(entrada, clienteC) {
         }
         l = l + 1
     }
+
+    // Retorna os dias de hospedagem 
     return clienteC
 }
 
+// Retorna o valor em cada hotel 
 function valor(hoteisC, clienteC) {
 
+    // ***** Verificar *******
     var j = 0
+    // Soma os dias hospedados avaliando o tipo de cliente e o dia da semana
     while (j < clienteC.dias.length) {
         var i = 0
+        // ***** Verificar *******
         while (i < hoteisC.length) {
+            // Cliente fidelidade e dia de semana  
             if (clienteC.tipo && clienteC.dias[j]) {
                 hoteisC[i].valor = hoteisC[i].valor + hoteisC[i].clientes_fidelidade[0]
-            } else if (clienteC.tipo && !clienteC.dias[j]) {
+            }
+            // Cliente fidelidade e final de semana 
+            else if (clienteC.tipo && !clienteC.dias[j]) {
                 hoteisC[i].valor = hoteisC[i].valor + hoteisC[i].clientes_fidelidade[1]
-            } else if (!clienteC.tipo && clienteC.dias[j]) {
+            }
+            // Cliente regular e dia de semana 
+            else if (!clienteC.tipo && clienteC.dias[j]) {
                 hoteisC[i].valor = hoteisC[i].valor + hoteisC[i].clientes_normais[0]
-            } else {
+            }
+            // Cliente regular e final de semana 
+            else {
                 hoteisC[i].valor = hoteisC[i].valor + hoteisC[i].clientes_normais[1]
             }
 
@@ -95,25 +118,32 @@ function valor(hoteisC, clienteC) {
         }
         j = j + 1
     }
-    // console.log(hoteisC)
-    // console.log(clienteC)
+
     return hoteisC, clienteC
 }
 
+// Retorna o melhor hotel mais barato
 function escolha(hoteisC, clienteC) {
+
+
+    // Variáveis auxiliares para comparação 
     var i = 0
     var escolhido = hoteisC[i]
 
+    // Percorre os hoteis buscando o mais barato 
     while (i < hoteisC.length) {
-
+        // Caso os hoteis tenham o mesmo valor, escolhe o com maior classificação 
         if (hoteisC[i].valor == escolhido.valor && hoteisC[i].classe > escolhido.classe) {
             escolhido = hoteisC[i]
-        } else if (hoteisC[i].valor < escolhido.valor) {
+        }
+        //Caso tenha um valor menor substitui o hotel escolhido pelo atual 
+        else if (hoteisC[i].valor < escolhido.valor) {
             escolhido = hoteisC[i]
         }
         i = i + 1
     }
 
+    // Retorna o hotel escolhido e seu valor 
     clienteC.valor = escolhido.valor
     clienteC.hotel = escolhido.nome
 
@@ -121,11 +151,8 @@ function escolha(hoteisC, clienteC) {
 
 }
 
+// Executa função 
+// hotel_escolhido('regular: 20Mar2009(sat)')
 
-
-
-hotel_escolhido('Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)')
-
-
-
+// Exporta função para testes 
 module.exports.hotel_escolhido = hotel_escolhido;
--- test/failing-test-spec.js
+++ test/failing-test-spec.js
@@ -1,9 +1,12 @@
-// 'use strict'
+'use strict'
 
 const chai = require('chai')
 const expect = chai.expect
+
+// Importa funções 
 const definido=require('../myLib/main')
 
+// Entradas 
 const dadosHotel=[
 'Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)',
 'Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)',
